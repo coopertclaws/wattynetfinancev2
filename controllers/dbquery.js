3df618ef37101b2d19dffd9cb81465c65cd74b52
Ben Watson <ben@wattyboys.com>
2020-08-16 12:08:11 +0100

    PUT method on virtual account for updates

--- controllers/dbquery.js
+++ controllers/dbquery.js
@@ -38,9 +38,28 @@ exports.createVirtualAccount = function(req, res, next) {
     });
 };
 
+exports.updateVirtualAccount = function(req, res, next) {
+    console.log('update account controller called');
+    var results = db.query("UPDATE virtual_account INNER JOIN user ON user.id = virtual_account.user SET virtual_account.amount = '" + req.body.amount + "', virtual_account.name = '" + req.body.name + "' WHERE (virtual_account.id = " + req.body.id + " AND user.email='" + req.userContext.userinfo.preferred_username + "')", function (error, results, fields) {
+        if (error) {
+            next(null,{
+                status: "error",
+                message: error
+            });
+        }
+
+        next({
+            status: "success",
+            message: "data retrieved",
+            data: results
+        }, null);
+    });
+};
+
+
 exports.getVirtualAccount = function(req, res, next) {
     console.log(req.query);
-    var results = db.query("SELECT * FROM virtual_account WHERE id = " + req.query.account, function (error, results, fields) {
+    var results = db.query("SELECT virtual_account.id, virtual_account.amount, virtual_account.current_balance, virtual_account.name, virtual_account.starting_balance, physical_account.name AS real_account FROM virtual_account INNER JOIN user ON user.id = virtual_account.user INNER JOIN physical_account ON physical_account.id = virtual_account.physical_account WHERE user.email = " + "'" + req.userContext.userinfo.preferred_username + "'" + " AND virtual_account.id = " + req.query.account, function (error, results, fields) {
         res.locals.results = results;
         if (error) {
             next(null,{
@@ -57,25 +76,6 @@ exports.getVirtualAccount = function(req, res, next) {
     });
   
 };
-
-// exports.getVirtualAccount = function(req, res, next) {
-//     var results = db.query("SELECT * FROM virtual_account WHERE id = " + req.params.id, function (error, results, fields) {
-//         res.locals.results = results;
-//         if (error) {
-//             next(null,{
-//                 status: "error",
-//                 message: error
-//             });
-//         }
-
-//         next({
-//             status: "success",
-//             message: "data retrieved",
-//             data: results
-//         }, null);
-//     });
-  
-// };
 
 exports.getAllVirtualAccounts = function(req, res, next) {
     var results = db.query("SELECT virtual_account.id, virtual_account.name, virtual_account.current_balance, virtual_account.amount, virtual_account.starting_balance, physical_account.name AS real_account FROM virtual_account INNER JOIN user ON user.id = virtual_account.user INNER JOIN physical_account ON physical_account.id = virtual_account.physical_account WHERE user.email = " + "'" + req.userContext.userinfo.preferred_username + "'", function(error, results, fields) {
@@ -94,19 +94,3 @@ exports.getAllVirtualAccounts = function(req, res, next) {
     });
 };
 
-// exports.getAllVirtualAccounts = function(req, res, next) {
-//     var results = db.query("SELECT * FROM virtual_account WHERE user = " + res.locals.user, function(error, results, fields) {
-//         if (error) {
-//             next(null, {
-//                 status: "error",
-//                 message: error
-//             });
-//         }
-
-//         next({
-//             status: "success",
-//             message: "data retreived",
-//             data: results
-//         }, null);
-//     });
-// };
--- routes/api/virtualaccount.js
+++ routes/api/virtualaccount.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var methodOverride = require('method-override');
 var query = require('../../controllers/dbquery');
 const session = require('express-session');
 const { ExpressOIDC } = require('@okta/oidc-middleware');
@@ -9,6 +10,8 @@ const client = new okta.Client({
   orgUrl: process.env.ORGURL,
   token: process.env.TOKEN
 });
+
+router.use(methodOverride('_method'));
 
 router.use((req, res, next) => {
   if (!req.userinfo) {
@@ -23,6 +26,9 @@ router.use((req, res, next) => {
       next(err);
     });
 });
+
+
+
 
 
 router.get('/', function(req, res) {
@@ -59,45 +65,6 @@ router.get('/', function(req, res) {
 
 });
 
-
-
-
-// router.get('/:id', function(req, res) {
-//     query.getVirtualAccount(req, res, function(data, error) {
-//         if(error) {
-//             res.send('Something Broke!');
-//         }
-//         else {
-//             // res.send(data.data[0].name);
-//             res.render('displayvirtualaccount', { title: 'Virtual Account Information',
-//                                                     name: data.data[0].name,
-//                                                     physical_account: data.data[0].physical_account,
-//                                                     starting_balance: data.data[0].starting_balance,
-//                                                     current_balance: data.data[0].current_balance,
-//                                                     amount: data.data[0].amount
-//                                                 });
-//         }
-//     })
-
-// });
-
-
-// router.get('/', function(req, res) {
-//     query.getAllVirtualAccounts(req, res, function(data, error) {
-//         if(error) {
-//             res.send('Something Broke!');
-//         }
-//         else {
-//             console.log(data);
-//             res.render('displayallvirtualaccounts', { title: 'Account Overview',
-//                                                     account_array: data.data
-//                                                 });
-//         }
-//     })
-
-// });
-
-
 router.post('/', function(req, res) {
     query.createVirtualAccount(req, res, function(data, error) {
         if(error) {
@@ -111,6 +78,18 @@ router.post('/', function(req, res) {
 
 });
 
+router.put('/', function(req, res) {
+    console.log('PUT request called');
+    query.updateVirtualAccount(req, res, function(data, error) {
+        if(error) {
+            res.send('Something Broke!');
+        }
+        else {
+            // res.send(data.data[0].name);
+            res.redirect('virtualaccount');
+        }
+    })
 
+});
 
 module.exports = router;
